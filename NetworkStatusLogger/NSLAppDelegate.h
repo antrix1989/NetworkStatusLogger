b5542e677b97d68d8b0e8a5b031f64e135e918fe
Sergey Demchenko <antrix1989@gmail.com>
2014-03-13 11:25:21 +0200

    Initial Commit

---
+++ NetworkStatusLogger/NSLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NSLAppDelegate.h
+//  NetworkStatusLogger
+//
+//  Created by Sergey Demchenko on 3/13/14.
+//  Copyright (c) 2014 Sergey Demchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NSLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NetworkStatusLogger/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NetworkStatusLogger
+//
+//  Created by Sergey Demchenko on 3/13/14.
+//  Copyright (c) 2014 Sergey Demchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NSLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NSLAppDelegate class]));
+    }
+}
